5bf85fc7579267cdffe583c94c3329ab62e46209
dafazenita <dafazenita.2019@student.uny.ac.id>
2021-09-21 13:32:07 +0700

    ver1

---
+++ lib/shared/theme_share.dart
@@ -0,0 +1,8 @@
+part of 'shared.dart';
+
+Color primaryColor = Color(0xFF2A1A5E);
+Color secondaryColor = Color(0xFFFB9224);
+Color dangerColor = Color(0xFFBB371A);
+Color blackColor = Color(0xFF11052C);
+Color whiteColor = Colors.white;
+
